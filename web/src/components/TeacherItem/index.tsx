39bcfbccddd1526f7b528f9f627945d3b5ede5f1
Vanderlei Kempf <dev.vjk@gmail.com>
2020-08-07 22:01:20 -0300

    :construction: Back End integrado com o Front End

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,56 @@
 import React from 'react';
+import api from '../../services/api';
 
 import wahtsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/62653064?s=460&u=5b09c79d69639189ad8b482fa9da1674319f1c27&v=4" alt="Vanderlei Kempf"/>
+                <img src={teacher.avatar} alt="{teacher.name}"/>
                 <div>
-                    <strong>Vanderlei Kempf</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p> Entusiasta das melhores tecnologias de quimica avançada.
-                <br/><br/>
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através das experiências.
+            <p> {teacher.bio}
             </p>
 
             <footer>
                 <p>
                 preço/hora
-                    <strong> R$ 80,00</strong>  
+                    <strong> R$ {teacher.cost}</strong>  
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={wahtsappIcon} alt="whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
 
         </article>
